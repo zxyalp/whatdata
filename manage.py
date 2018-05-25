75d1dca18dc62b587ce89bbc5a84b85c1e2ca064
xing yan <zgyan012@gmail.com>
2018-05-25 10:03:11 +0800

    add demo flask

--- manage.py
+++ manage.py
@@ -1,13 +1,22 @@
-from flask_script import Manager
+from flask_script import Manager, Shell
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_migrate import MigrateCommand
 
-from sayhello import app
+from sayhello import app, Role, User, db
 
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
 
 
 @manager.command
--- sayhello.py
+++ sayhello.py
@@ -1,23 +1,61 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, url_for, redirect, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = ' hand to guess string'
+app.config['SECRET_KEY'] = 'hSwand to guess string'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>'% self.username
 
 
 @app.route('/', methods=('GET', 'POST'))
-def hello_world():
-    name = None
+def index():
     form = UserForm()
     if form.validate_on_submit():
-        name = form.name.data
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+            flash('添加新用户成功')
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
         form.name.data = ''
-
-    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=name)
+        return redirect(url_for('index'))
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'),
+                           known = session.get('known', False))
 
 
 @app.route('/user/<name>')
